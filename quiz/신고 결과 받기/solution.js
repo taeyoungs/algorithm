9af1b384dd30556c5a931de7af58941c47703c1f
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-10-18 16:56:06 +0900

    mod: 신고 결과 받기 풀이 방법 업데이트

--- quiz/신고 결과 받기/solution.js
+++ quiz/신고 결과 받기/solution.js
@@ -1,4 +1,58 @@
+// #1 class 이용해서 구한 방식
+
+class Reporter {
+  constructor(id) {
+    this.id = id;
+    this.idThatReportedMe = new Set();
+    this.numberOfEmail = 0;
+  }
+
+  addIdThatReportedMe(id) {
+    this.idThatReportedMe.add(id);
+  }
+
+  plusEmailCount() {
+    this.numberOfEmail += 1;
+  }
+
+  isSuspened(count) {
+    return this.idThatReportedMe.size >= count;
+  }
+
+  isAleadyReported(id) {
+    return this.idThatReportedMe.has(id);
+  }
+}
+
 function solution(id_list, report, k) {
+  const reporters = {};
+
+  for (let i = 0; i < id_list.length; i++) {
+    reporters[id_list[i]] = new Reporter(id_list[i]);
+  }
+
+  for (let i = 0; i < report.length; i++) {
+    const [reporter, target] = report[i].split(' ');
+
+    if (!reporters[reporter].isAleadyReported()) {
+      reporters[target].addIdThatReportedMe(reporter);
+    }
+  }
+
+  for (let i = 0; i < id_list.length; i++) {
+    const user = reporters[id_list[i]];
+
+    if (user.isSuspened(k)) {
+      user.idThatReportedMe.forEach((id) => reporters[id].plusEmailCount());
+    }
+  }
+
+  return Array.from(Object.values(reporters)).map((reporter) => reporter.numberOfEmail);
+}
+
+// #2 class 이용하지 않은 방식
+
+function solution2(id_list, report, k) {
   const reporters = {};
   const emailTable = {};
 
