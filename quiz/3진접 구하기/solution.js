b64a9c4fd90fe2c9e6975ba884f26e742bbd5e57
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-10-20 23:06:43 +0900

    mod: Number.parseInt 활용한 풀이 방법 추가

--- quiz/3진접 구하기/solution.js
+++ quiz/3진접 구하기/solution.js
@@ -1,4 +1,5 @@
-function solution(n) {
+// #1
+function solution1(n) {
   let num = n;
   const arr = [];
 
@@ -11,4 +12,19 @@ function solution(n) {
 
   let len = arr.length - 1;
   return arr.reduce((acc, cur) => (acc += 3 ** len-- * cur), 0);
+}
+
+// #2 Number.parseInt 활용한 방식
+function solution1(n) {
+  let num = n;
+  const arr = [];
+
+  while (num >= 3) {
+    arr.push(num % 3);
+
+    num = Math.floor(num / 3);
+  }
+  arr.push(num);
+
+  return +Number.parseInt(arr.join(''), 3);
 }
